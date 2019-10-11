650aba936910d997b85e6149997ea4a69be44d53
Yifan <yifanh@cmu.edu>
2019-10-11 13:49:22 -0400

    Implemented base class for Delta robot

---
+++ robots/delta_dji/include/delta_dji/delta_dji.h
@@ -0,0 +1,13 @@
+#ifndef _DELTA_DJI_CLASS_HEADER_
+#define _DELTA_DJI_CLASS_HEADER_
+
+#include "hardware_interfaces/delta_interfaces.h"
+
+class DeltaDJI: public DeltaInterfaces {
+public:
+  DeltaDJI();
+  ~DeltaDJI();
+  
+};
+
+#endif
