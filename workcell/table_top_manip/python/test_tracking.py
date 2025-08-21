34962705f7c1b4ff7860c54ad4f0ecebb87c6d78
Yifan Hou <yifanhou@stanford.edu>
2025-08-21 13:50:04 -0700

    Updated test tracking script

--- workcell/table_top_manip/python/test_tracking.py
+++ workcell/table_top_manip/python/test_tracking.py
@@ -21,7 +21,7 @@ print("[python] creating manip server")
 reset_all_elgato_devices()
 server = ms.ManipServer()
 if not server.initialize(
-    "/home/yifanhou/git/hardware_interfaces_internal/workcell/table_top_manip/config/single_arm_evaluation.yaml"
+    "/home/chendong/workspace/hardware_interfaces/workcell/table_top_manip/config/single_arm_evaluation.yaml"
 ):
     raise RuntimeError("Failed to initialize server")
 server.set_high_level_maintain_position()
@@ -32,6 +32,8 @@ while not server.is_ready():
     sleep(1)
 print("[python] Server is ready")
 
+# calibrate
+server.calibrate_robot_wrench(100)
 pose_fb = server.get_pose(1)
 print("[python] pose_fb:", pose_fb)
 
@@ -41,8 +43,8 @@ print("[python] done set_force_controlled_axis")
 pose_cmd = copy.deepcopy(pose_fb)
 
 N = 120
-magnitude = 0 #0.05
-dt_ms = 50  # milliseconds
+magnitude = 0.05
+dt_ms = 30  # milliseconds
 pose_cmds = np.zeros((N, 7))
 t = np.linspace(0, 2 * np.pi, N)
 step = np.concatenate([np.zeros(N//3), np.ones(N//3), np.zeros(N//3)])  # 0 for first half, 1 for second half
@@ -64,14 +66,14 @@ q_all = np.concatenate([Ns @ q_fb, Ns @ q_cmd, Ns @ q_fb], axis=0)
 
 # pose_cmds[:, 0] = pose_fb[0] + magnitude * np.sin(t)  # x position
 # pose_cmds[:, 0] = pose_fb[0] + magnitude * step  # x position
-pose_cmds[:, 0] = pose_fb[0] + magnitude * step  # x position
-pose_cmds[:, 1] = pose_fb[1] + magnitude * step  # y position
-pose_cmds[:, 2] = pose_fb[2] + magnitude * step  # z position
-# pose_cmds[:, 3] = pose_fb[3]  # quaternion x
-# pose_cmds[:, 4] = pose_fb[4]  # quaternion y
-# pose_cmds[:, 5] = pose_fb[5]  # quaternion z
-# pose_cmds[:, 6] = pose_fb[6]  # quaternion w
-pose_cmds[:, 3:] = q_all
+pose_cmds[:, 0] = pose_fb[0] + magnitude * np.sin(t)  # x position
+pose_cmds[:, 1] = pose_fb[1] + magnitude * np.sin(t)  # y position
+pose_cmds[:, 2] = pose_fb[2] + magnitude * np.sin(t)  # z position
+pose_cmds[:, 3] = pose_fb[3]  # quaternion x
+pose_cmds[:, 4] = pose_fb[4]  # quaternion y
+pose_cmds[:, 5] = pose_fb[5]  # quaternion z
+pose_cmds[:, 6] = pose_fb[6]  # quaternion w
+# pose_cmds[:, 3:] = q_all
 
 log_pose_x = []
 log_pose_y = []
@@ -84,7 +86,7 @@ deltas = np.array([0.00, -0.00])
 timepoints_cmd_ms = (
     server.get_timestamp_now_ms() + np.arange(N) * dt_ms
 )
-server.schedule_waypoints(pose_cmds, timepoints_cmd_ms)
+server.schedule_waypoints(pose_cmds.T, timepoints_cmd_ms)
 
 for i in range(N):
     pose_fb = server.get_pose(1)
@@ -96,11 +98,14 @@ for i in range(N):
     sleep(dt_ms / 1000.0)  # Convert milliseconds to seconds
 
 
-
 server.join_threads()
 print("start drawing")
 
-fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+x_error = np.abs(log_pose_x[-1] - pose_cmds[-1, 0])
+z_error = np.abs(log_pose_z[-1] - pose_cmds[-1, 2])
+print("X_error: ", x_error, ", z_error: ", z_error)
+
+fig, axs = plt.subplots(1, 3, figsize=(12, 5), sharey=False)
 
 # plot log_pose_x, log_pose_y, log_pose_z
 axs[0].plot(log_time, log_pose_x, label="pose_x")
@@ -114,5 +119,10 @@ axs[2].plot(timepoints_cmd_ms, pose_cmds[:, 2], label="pose_cmd_z")
 axs[0].legend()
 axs[1].legend()
 axs[2].legend()
+
+# enable grid
+axs[0].grid()
+axs[1].grid()
+axs[2].grid()
 
 plt.show()
